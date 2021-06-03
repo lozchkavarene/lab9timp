b162874595c3d47460e46e18401892148b31d5df
lozchkavarene <71684607+lozchkavarene@users.noreply.github.com>
2021-06-03 17:05:49 +0300

    Delete Report_09 directory

--- Report_09/lib/print.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-void print (std::string& str, std::ostream& out)
-{
-    out << str;
-}
--- Report_09/lib/print.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef PRINT_H
-#define PRINT_H
-#include <iostream>
-
-void print (std::string&, std::ostream&);
-
-#endif
